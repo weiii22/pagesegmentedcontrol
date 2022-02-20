0207b9206496087d7a0dc4331cc61c437cfb8777
wei22 <weiwang24@icloud.com>
2022-02-20 09:13:06 +0800

    Initial Commit

---
+++ pagesegmentedcontrol/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pagesegmentedcontrol
+//
+//  Created by 王偉程 on 2022/2/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
